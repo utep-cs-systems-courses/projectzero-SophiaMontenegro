fd461a42cff41495ccfad1b7f88c9e57f6ebb6b2
SophiaMontenegro <sophiaazulmonetenegro@gmail.com>
2023-09-18 17:10:06 -0600

    Final

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c or 2, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -27,7 +27,12 @@ int main()
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+	print_char_11x16(c);      
+      break;
+    case '2':
+      puts("You selected chars with font 8x12:");
+      for(char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
       break;
     case 'a':
       puts("You selected arrow:");
